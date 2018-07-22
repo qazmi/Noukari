ffc061769f6eff9971fee97ea19161771f827892
Ali Kazmi <ali_kazmi@elixir.com>
2018-07-22 10:55:32 +0500

    modified get

--- Server/server.js
+++ Server/server.js
@@ -18,10 +18,10 @@ app.use(fileUpload());
 app.use(express.static(__dirname + '/../dist/Noukari'));
 
 
-// app.get('/', (req, res) => {
+app.get('/', (req, res) => {
 
-//   res.sendFile(path.join(__dirname + '/../dist/Noukari/index.html'));
-// })
+  res.sendFile(path.join(__dirname + '/../dist/Noukari/index.html'));
+})
 
 app.post('/login-email', async (req, res) => {
 
